94f0857e4abfcd7e26b9e254c0ea804edd726d57
Yagaichi <ichiyaga.21@gmail.com>
2024-06-25 10:31:03 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+document.addEventListener("alpine:init", () => {
+  Alpine.data("products", () => ({
+    items: [
+      { id: 1, name: "Putri Salju", img: "image1.jpg", price: 40000 },
+      { id: 2, name: "Castangel", img: "image2.jpg", price: 45000 },
+      { id: 3, name: "Nastar", img: "image3.jpg", price: 40000 },
+      { id: 4, name: "Confleke", img: "image4.jpg", price: 45000 },
+      { id: 5, name: "CheseStik", img: "image5.jpg", price: 50000 },
+    ],
+  }));
+
+  Alpine.store("cart", {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+      // cek apakah ada brg yg sama
+      const cartItem = this.items.find((item) => item.id === newItem.id);
+      if (!cartItem) {
+        this.items.push({ ...newItem, quantity: 1, total: newItem.price });
+        this.quantity++;
+        this.total += newItem.price;
+      } else {
+        this.items = this.items.map((item) => {
+          if (item.id !== newItem.id) {
+            return item;
+          } else {
+            item.quantity++;
+            item.total = item.price * item.quantity;
+            this.quantity++;
+            this.total += item.price;
+            return item;
+          }
+        });
+      }
+    },
+    remove(id) {
+      const cartItem = this.items.find((item) => item.id === id);
+      if (cartItem.quantity > 1) {
+        this.items = this.items.map((item) => {
+          if (item.id !== id) {
+            return item;
+          } else {
+            item.quantity--;
+            item.total = item.price * item.quantity;
+            this.quantity--;
+            this.total -= item.price;
+            return item;
+          }
+        });
+      } else if (cartItem.quantity === 1) {
+        this.items = this.items.filter((item) => item.id !== id);
+        this.quantity--;
+        this.total -= cartItem.price;
+      }
+    },
+  });
+});
+
+//rupiah
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+};
